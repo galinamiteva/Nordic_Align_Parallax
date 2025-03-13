da2aca397d632afa057925016ba81410dcfd029e
Gala Miteva <galina.miteva@utb.ecutbildning.se>
2025-03-13 21:03:09 +0100

    scss borjan

---
+++ Nordic_Align_V7/wwwroot/js/app.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener("mousemove", (event) => {
+        document.documentElement.style.setProperty(
+            "--move-x",
+            `${(event.clientX - window.innerWidth / 2) * -0.005}deg`
+        );
+        document.documentElement.style.setProperty(
+            "--move-y",
+            `${(event.clientY - window.innerHeight / 2) * -0.01}deg`
+        );
+    });
+});
---
+++ Nordic_Align_V7/wwwroot/js/main.js
@@ -0,0 +1,43 @@
+﻿
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("main.js are here");
+
+    // Бургер меню
+    const BurgerBtn = document.querySelector(".burger");
+    const BurgerMenu = document.querySelector(".menu");
+
+    if (BurgerBtn && BurgerMenu) {
+        BurgerBtn.addEventListener("click", function () {
+            this.classList.toggle("burger--active");
+            BurgerMenu.classList.toggle("menu--active");
+        });
+    } else {
+        console.error("Burger button or menu not found!");
+    }
+
+
+    document.addEventListener("scroll", function () {
+        document.body.style.setProperty("--scrollTop", `${window.scrollY}px`);
+    });
+
+
+    const menuLinks = document.querySelectorAll(".menu__list-link");
+
+    menuLinks.forEach((link) => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 50,
+                    behavior: "smooth",
+                });
+            }
+        });
+    });
+});
+
